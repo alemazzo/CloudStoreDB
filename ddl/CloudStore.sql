0b16d6c9be3df9549046d0fef8494df11acc2a09
Alessandro Mazzoli <Alemazzoli97@gmail.com>
2021-06-16 10:20:06 +0200

    Added DDL

--- ddl/CloudStore.sql
+++ ddl/CloudStore.sql
@@ -3,213 +3,210 @@
 # Create tables
 CREATE TABLE IF NOT EXISTS Directory
 (
-    id INT NOT NULL AUTOINCREMENT,
-    nome VARCHAR(20) NOT NULL,
-    data_creazione DATETIME NOT NULL,
-    padre INT,
-    propietario VARCHAR(50) NOT NULL,
-    PRIMARY KEY(ID),
-    UNIQUE(padre, nome, proprietario)
+    Id INT NOT NULL,
+    Nome VARCHAR(20) NOT NULL,
+    DataCreazione DATETIME NOT NULL,
+    Padre INT,
+    Proprietario VARCHAR(50) NOT NULL,
+    PRIMARY KEY(Id),
+    UNIQUE(Padre, Nome)
 );
 
 CREATE TABLE IF NOT EXISTS File
 (
-    id INT NOT NULL,
-    directory INT NOT NULL,
-    nome VARCHAR(0) NOT NULL,
-    estensione VARCHAR(0) NOT NULL,
-    proprietario VARCHAR(50) NOT NULL,
-    numero_ultima_versione INT NOT NULL,
+    Id INT NOT NULL,
+    Directory INT NOT NULL,
+    Nome VARCHAR(30) NOT NULL,
+    Estensione VARCHAR(3) NOT NULL,
+    Proprietario VARCHAR(50) NOT NULL,
+    UltimaVersione INT NOT NULL,
     PRIMARY KEY(id),
-    UNIQUE(directory, nome, estensione)
-    
+    UNIQUE(Directory, Nome, Estensione)
 );
 
 CREATE TABLE IF NOT EXISTS Utenti
 (
-    email VARCHAR(50) NOT NULL,
-    nome VARCHAR(20) NOT NULL,
-    cognome VARCHAR(20) NOT NULL,
-    data_registrazione DATE NOT NULL,
-    password VARCHAR(20) NOT NULL,
-    data_di_nascita DATE NOT NULL,
-    numero_directory INT NOT NULL,
-    PRIMARY KEY(email)
+    Email VARCHAR(50) NOT NULL,
+    Nome VARCHAR(20) NOT NULL,
+    Cognome VARCHAR(20) NOT NULL,
+    DataRegistrazione DATE NOT NULL,
+    Password VARCHAR(20) NOT NULL,
+    DataNascita DATE NOT NULL,
+    NumeroDirectory INT NOT NULL,
+    PRIMARY KEY(Email)
 );
 
 CREATE TABLE IF NOT EXISTS Preferenze
 (
-    id_file INT NOT NULL,
-    utente VARCHAR(50) NOT NULL,
-    data DATETIME NOT NULL,
-    PRIMARY KEY(id_file, utente)
+    File INT NOT NULL,
+    Utente VARCHAR(50) NOT NULL,
+    Data DATETIME NOT NULL,
+    PRIMARY KEY(File, Utente)
 );
 
 CREATE TABLE IF NOT EXISTS Versioni
 (
-    id INT NOT NULL,
-    file INT NOT NULL,
-    numero INT NOT NULL,
-    data DATETIME NOT NULL,
-    dimensione INT NOT NULL,
-    link VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
+    Id INT NOT NULL,
+    File INT NOT NULL,
+    Numero INT NOT NULL,
+    Data DATETIME NOT NULL,
+    Dimensione INT NOT NULL,
+    Link VARCHAR(30) NOT NULL,
+    PRIMARY KEY(Id),
+    UNIQUE(File, Numero)
 );
 
 CREATE TABLE IF NOT EXISTS Downloads
 (
-    versione_numero INT NOT NULL,
-    versione_file INT NOT NULL,
-    utente VARCHAR(50) NOT NULL,
-    data DATETIME NOT NULL,
-    PRIMARY KEY(versione_numero, versione_file, utente)
+    Versione INT NOT NULL,
+    Utente VARCHAR(50) NOT NULL,
+    Data DATETIME NOT NULL,
+    PRIMARY KEY(Versione, Utente)
 );
 
 CREATE TABLE IF NOT EXISTS Visualizzazioni
 (
-    id_versione INT NOT NULL,
-    utente VARCHAR(50) NOT NULL,
-    data DATETIME NOT NULL,
-    PRIMARY KEY(id_versione, utente)
+    Versione INT NOT NULL,
+    Utente VARCHAR(50) NOT NULL,
+    Data DATETIME NOT NULL,
+    PRIMARY KEY(Versione, Utente)
 );
 
 CREATE TABLE IF NOT EXISTS Segnalazioni
 (
-    ID INT NOT NULL,
-    utente VARCHAR(50) NOT NULL,
-    descrizione VARCHAR(0) NOT NULL,
-    operatore INT,
-    data_accettazione DATETIME,
-    data_chiusura DATETIME,
+    Id INT NOT NULL,
+    Utente VARCHAR(50) NOT NULL,
+    Descrizione VARCHAR(0) NOT NULL,
+    Operatore INT,
+    DataAccettazione DATETIME,
+    DataChiusura DATETIME,
     PRIMARY KEY(ID)
 );
 
 CREATE TABLE IF NOT EXISTS Operatori
 (
-    codice INT NOT NULL,
-    nome VARCHAR(0) NOT NULL,
-    password VARCHAR(0) NOT NULL,
-    data_di_nascita DATE NOT NULL,
-    PRIMARY KEY(codice)
+    Codice INT NOT NULL,
+    Nome VARCHAR(0) NOT NULL,
+    Password VARCHAR(0) NOT NULL,
+    DataNascita DATE NOT NULL,
+    PRIMARY KEY(Codice)
 );
 
 CREATE TABLE IF NOT EXISTS Interventi
 (
-    segnalazione INT NOT NULL,
-    numero INT NOT NULL,
-    utente VARCHAR(50),
-    operatore INT,
-    messaggio VARCHAR(0) NOT NULL,
-    data DATETIME NOT NULL,
-    PRIMARY KEY(segnalazione, numero)
+    Segnalazione INT NOT NULL,
+    Numero INT NOT NULL,
+    Utente VARCHAR(50),
+    Operatore INT,
+    Messaggio VARCHAR(0) NOT NULL,
+    Data DATETIME NOT NULL,
+    PRIMARY KEY(Segnalazione, Numero),
+    CONSTRAINT GERARCHIA_INTERVENTI CHECK (
+    	(Operatore is null or Utente is null) 
+    	and not 
+    	(Operatore is null and Utente is null)
+   	)
+
 );
 
 CREATE TABLE IF NOT EXISTS Condivisioni
 (
-    id_file INT NOT NULL,
-    utente VARCHAR(50) NOT NULL,
-    lettura TINYINT(1),
-    scrittura TINYINT(1),
-    data DATE,
-    PRIMARY KEY(id_file, utente)
+    File INT NOT NULL,
+    Utente VARCHAR(50) NOT NULL,
+    Lettura TINYINT(1),
+    Scrittura TINYINT(1),
+    Data DATE,
+    PRIMARY KEY(File, Utente)
 );
 
 
 # Create FKs
 ALTER TABLE File
-    ADD    FOREIGN KEY (directory)
-    REFERENCES Directory(ID)
+    ADD    FOREIGN KEY (Directory)
+    REFERENCES Directory(Id)
 ;
     
 ALTER TABLE Directory
-    ADD    FOREIGN KEY (padre)
-    REFERENCES Directory(ID)
+    ADD    FOREIGN KEY (Padre)
+    REFERENCES Directory(Id)
 ;
     
 ALTER TABLE Directory
-    ADD    FOREIGN KEY (propietario)
-    REFERENCES Utenti(email)
+    ADD    FOREIGN KEY (Proprietario)
+    REFERENCES Utenti(Email)
 ;
     
 ALTER TABLE Preferenze
-    ADD    FOREIGN KEY (utente)
-    REFERENCES Utenti(email)
+    ADD    FOREIGN KEY (Utente)
+    REFERENCES Utenti(Email)
 ;
     
 ALTER TABLE Downloads
-    ADD    FOREIGN KEY (versione_file)
-    REFERENCES Versioni(file)
+    ADD    FOREIGN KEY (Versione)
+    REFERENCES Versioni(Id)
 ;
-    
-ALTER TABLE Downloads
-    ADD    FOREIGN KEY (versione_numero)
-    REFERENCES Versioni(numero)
-;
-    
-ALTER TABLE Downloads
-    ADD    FOREIGN KEY (utente)
-    REFERENCES Utenti(email)
-;
-    
-ALTER TABLE Visualizzazioni
-    ADD    FOREIGN KEY (utente)
-    REFERENCES Utenti(email)
-;
-    
-ALTER TABLE Segnalazioni
-    ADD    FOREIGN KEY (utente)
-    REFERENCES Utenti(email)
-;
-    
-ALTER TABLE Segnalazioni
-    ADD    FOREIGN KEY (operatore)
-    REFERENCES Operatori(codice)
-;
-    
-ALTER TABLE Interventi
-    ADD    FOREIGN KEY (segnalazione)
-    REFERENCES Segnalazioni(ID)
-;
-    
-ALTER TABLE Interventi
-    ADD    FOREIGN KEY (utente)
-    REFERENCES Utenti(email)
-;
-    
-ALTER TABLE Interventi
-    ADD    FOREIGN KEY (operatore)
-    REFERENCES Operatori(codice)
-;
-    
-ALTER TABLE Condivisioni
-    ADD    FOREIGN KEY (utente)
-    REFERENCES Utenti(email)
-;
-    
-ALTER TABLE File
-    ADD    FOREIGN KEY (proprietario)
-    REFERENCES Utenti(email)
-;
-    
-ALTER TABLE File
-    ADD    FOREIGN KEY (numero_ultima_versione)
-    REFERENCES Versioni(numero)
-;
-    
-ALTER TABLE Preferenze
-    ADD    FOREIGN KEY (id_file)
-    REFERENCES File(id)
-;
-    
-ALTER TABLE Condivisioni
-    ADD    FOREIGN KEY (id_file)
-    REFERENCES File(id)
-;
-    
-ALTER TABLE Visualizzazioni
-    ADD    FOREIGN KEY (id_versione)
-    REFERENCES Versioni(id)
-;
-    
 
-# Create Indexes
+ALTER TABLE Downloads
+    ADD    FOREIGN KEY (Utente)
+    REFERENCES Utenti(Email)
+;
+    
+ALTER TABLE Visualizzazioni
+    ADD    FOREIGN KEY (Utente)
+    REFERENCES Utenti(Email)
+;
+    
+ALTER TABLE Segnalazioni
+    ADD    FOREIGN KEY (Utente)
+    REFERENCES Utenti(Email)
+;
+    
+ALTER TABLE Segnalazioni
+    ADD    FOREIGN KEY (Operatore)
+    REFERENCES Operatori(Codice)
+;
+    
+ALTER TABLE Interventi
+    ADD    FOREIGN KEY (Segnalazione)
+    REFERENCES Segnalazioni(Id)
+;
+    
+ALTER TABLE Interventi
+    ADD    FOREIGN KEY (Utente)
+    REFERENCES Utenti(Email)
+;
+    
+ALTER TABLE Interventi
+    ADD    FOREIGN KEY (Operatore)
+    REFERENCES Operatori(Codice)
+;
+    
+ALTER TABLE Condivisioni
+    ADD    FOREIGN KEY (Utente)
+    REFERENCES Utenti(Email)
+;
+    
+ALTER TABLE File
+    ADD    FOREIGN KEY (Proprietario)
+    REFERENCES Utenti(Email)
+;
+    
+ALTER TABLE File
+    ADD    FOREIGN KEY (UltimaVersione)
+    REFERENCES Versioni(Id)
+;
+    
+ALTER TABLE Preferenze
+    ADD    FOREIGN KEY (File)
+    REFERENCES File(Id)
+;
+    
+ALTER TABLE Condivisioni
+    ADD    FOREIGN KEY (File)
+    REFERENCES File(Id)
+;
+    
+ALTER TABLE Visualizzazioni
+    ADD    FOREIGN KEY (Versione)
+    REFERENCES Versioni(Id)
+;
